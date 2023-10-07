9496c26392d8ce33aef6b24f7a2fcbc0f7c54773
tejasvi raj <tejasviraj8686@gmail.com>
2023-10-07 15:17:29 +0530

    function

---
+++ Function/HomeWork/4function.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+bool checkPrime(int n) {
+    if (n <= 1) {
+        return false; // Numbers less than or equal to 1 are not prime
+    }
+
+    for (int i = 2; i < n; i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    bool isPrime = checkPrime(n);
+    if (isPrime) {
+        cout << "Prime\n";
+    } else {
+        cout << "Not Prime\n";
+    }
+    return 0;
+}
