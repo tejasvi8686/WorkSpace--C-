ab93f773fb3958efc69f039d6d343a074cf8fa55
tejasvi raj <tejasviraj8686@gmail.com>
2023-10-21 00:58:42 +0530

    array

--- Array/9Array.cpp
+++ Array/9Array.cpp
@@ -2,8 +2,27 @@
 #include <limits.h>
 using namespace std;
 
-int main () {
+int findMinimumInArray(int arr[], int size)
+{
+    // ans store variable
+    int minAns = INT_MAX;
 
-    cout << INT_MIN << endl;
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] < minAns)
+        {
+            minAns = arr[i];
+        }
+    }
+    return minAns;
+}
 
+int main()
+{
+
+    int arr[] = {10, 8, 31, 4, 3, 2, 51};
+    int size = 7;
+
+    int minimum = findMinimumInArray(arr, size);
+    cout << "The smallest element in the array is: " << minimum;
 }
